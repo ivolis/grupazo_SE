b2ead6ebe797d1387445f22af9561bacc6077bde
ivolis <39422659+ivolis@users.noreply.github.com>
2022-09-04 19:05:57 -0300

    entrega

--- main_guia_1_02_inc2.cpp
+++ main_guia_1_02_inc2.cpp
@@ -1,19 +1,19 @@
-#include "mbed.h"
-#include "arm_book_lib.h"
+#include "mbed.h" // inclusión de libreria mbed.h
+#include "arm_book_lib.h" // inclusión de libreria arm_book_lib.h
 
 int main()
 {
-    DigitalIn gasDetector(D2); // declaro pin D2 como entrada y le asocio el nombre gasDetector (constructor)
-    // En C se asocia gpio_t a D2 como tipo de dato
-    DigitalOut alarmLed(LED1); // declaro LED1 como salida (constructor)
+    DigitalIn gasDetector(D2); // (llamada al constructor) declaro pin D2 como entrada y le asocio el nombre gasDetector
+
+    DigitalOut alarmLed(LED1); // (llamada al constructor) declaro LED1 como salida y le asocio el nombre alarmLed
 
     gasDetector.mode(PullDown); // utilizo el metodo "mode" del objeto gasDetector para configurarle la resistencia de pulldown
-    // en este caso hay que declarar el prototipo y la implementacion
+
 
     alarmLed = OFF; // la clase tiene el operador "=" sobrecargado para asociarle un estado al objeto
     int n = 0; // inicializamos un int para usarlo de contador
 
-    while (true) {
+    while (true) { // loop que se repetira continuamente en la placa
         n++; // incrementamos el contador
         printf("Iteracion numero %d \n", n); // se imprime el contador
         if ( gasDetector == ON ) {
